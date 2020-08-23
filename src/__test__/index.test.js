1908e4961ee1aaaafae5f76ae3fc5339af81d593
Gabriel Cesar <gabrielcesar.a.l@gmail.com>
2020-08-23 17:01:25 -0300

    add tests

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,11 +1,38 @@
-const build = require("../index");
+const path = require("path");
+const gitProperties = require("../index");
 
-build
-  .buildGitInfo()
-  .then((it) => console.log(it))
-  .catch((err) => console.error(err));
+it("should retrieve branch", async () => {
+  const branch = await gitProperties.retrieveBranch();
+  expect(branch).not.toBeNull();
+  expect(branch).not.toBe("UNKNOWN");
+});
 
-build
-  .default()
-  .then((_) => console.log("FINISHED"))
-  .catch((err) => console.error(err));
+it("should retrieve commit info", async () => {
+  const { message, id, user } = await gitProperties.retrieveCommit();
+  expect(id).not.toBeNull();
+  expect(user).not.toBeNull();
+  expect(message).not.toBeNull();
+  expect(user.name).not.toBeNull();
+  expect(user.email).not.toBeNull();
+
+  expect(id).toHaveLength(40);
+  expect(message.length).toBeGreaterThan(2);
+});
+
+it("should retrieve general info", async () => {
+  await gitProperties.default();
+
+  const resultPath = require(path.resolve("./") + "/git.properties.json");
+
+  expect(resultPath).toHaveProperty("branch");
+  expect(resultPath).toHaveProperty("commit");
+
+  expect(resultPath.commit.id).not.toBeNull();
+  expect(resultPath.commit.user).not.toBeNull();
+  expect(resultPath.commit.message).not.toBeNull();
+  expect(resultPath.commit.user.name).not.toBeNull();
+  expect(resultPath.commit.user.email).not.toBeNull();
+
+  expect(resultPath.commit.id).toHaveLength(40);
+  expect(resultPath.commit.message.length).toBeGreaterThan(2);
+});
